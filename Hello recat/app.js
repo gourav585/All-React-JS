8c0fb1710e6ba0885b3a52429aa088ebb1c1fc26
Gourav Rajak <148696915+gourav585@users.noreply.github.com>
2025-01-08 11:56:24 +0530

    Add files via upload

---
+++ Hello recat/app.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", { id: "heading",class:"gourav" }, "hello world from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);  // render the heading which is creat by const heading rendering in page 
